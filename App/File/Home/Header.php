2d13edcbc201b105b73a7627ff088c4fb5b48fda
geraldon1997 <geraldon1997@gmail.com>
2020-05-10 07:56:30 -0400

    building the home page

--- App/File/Home/Header.php
+++ App/File/Home/Header.php
@@ -50,3 +50,47 @@
 
 </head>
 <body>
+<div id="wrapper">
+        <header class="market-header header">
+            <div class="container-fluid">
+                <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="https://bossearn.com/"><img src="<?php echo $assets; ?>images/version/bossearn.png" alt="" width="200"></a>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://bossearn.com/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://bossearn.com/dashboard.php/">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://bossearn.com/vendors.php/">Vendors</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">how it works</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://bossearn.com/login.php">Login</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="text" placeholder="How may I help?">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div><!-- end container-fluid -->
+        </header><!-- end market-header -->
+
+        <div class="row">
+            <div class="col-md-12" style="border: 1px solid black; background-color: orange;">
+                <img src="<?php echo $assets; ?>images/version/bossearn.png" alt="bossearn" width="100%" height="100" style="margin-top: 55px; filter: blur(10px);">
+            </div>    
+        </div><!-- end page-title -->
+        <marquee><h2> Welcome to Bossearn</h2></marquee>
