c751b589a6b766ffef838d9e510886ce106cb974
user <user@kali.kali.org>
2020-06-12 03:34:51 -0400

    done with the layout of the profile page

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php require_once 'layout/header.php' ?>
+
+<style>
+    #bg{
+        margin-top: 50px;
+        width: 100%;
+        height: 500px;
+    }
+    .btn-news:hover{
+        cursor: pointer;
+        color: gold !important;
+    }
+</style>
+<img src="App/Assets/Images/b1.jpeg" alt="" id="bg">
+        <section class="section lb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+                        <div class="page-wrapper">
+                            <div class="blog-custom-build">
+                                <div class="blog-box wow fadeIn">
+                                    <div class="post-media">
+                                        <a href="marketing-single.html" title="">
+                                            <img src="App/Assets/Images/market_blog_01.jpg" alt="" class="img-fluid">
+                                            <div class="hovereffect">
+                                                <span></span>
+                                            </div>
+                                            <!-- end hover -->
+                                        </a>
+                                    </div>
+                                    <!-- end media -->
+                                    <div class="blog-meta big-meta text-center">
+                                        <div class="post-sharing">
+                                            <ul class="list-inline">
+                                            <?php if (isset($_SESSION['uname'])) {?>
+                                                <li><share-button>share</share-button></li>
+                                            <?php } ?>
+                                            </ul>
+                                        </div><!-- end post-sharing -->
+                                        <h4>You can learn how to make money with your blog and videos</h4>
+                                        <form action="news.php" method="post">
+                                            <input type="hidden" name="news" value="1">
+                                            <button class="btn btn-news">Read more &nbsp;<i class="fa fa-book"></i></button>
+                                        </form>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+                                
+                            </div>
+                        </div>
+
+                        <hr class="invis">
+
+                        
+                    </div><!-- end col -->
+
+                    <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                        <div class="sidebar">
+                            
+
+                            <div class="widget">
+                                <h2 class="widget-title">Recent Posts</h2>
+                                <div class="blog-list-widget">
+                                    <div class="list-group">
+                                        <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="w-100 justify-content-between">
+                                                <img src="App/Assets/Images/small_07.jpg" alt="" class="img-fluid float-left">
+                                                <h5 class="mb-1">5 Beautiful buildings you need to before dying</h5>
+                                                <small>12 Jan, 2016</small>
+                                            </div>
+                                        </a>
+
+                                        <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="w-100 justify-content-between">
+                                                <img src="App/Assets/Images/small_08.jpg" alt="" class="img-fluid float-left">
+                                                <h5 class="mb-1">Let's make an introduction for creative life</h5>
+                                                <small>11 Jan, 2016</small>
+                                            </div>
+                                        </a>
+
+                                        <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
+                                            <div class="w-100 last-item justify-content-between">
+                                                <img src="App/Assets/Images/small_09.jpg" alt="" class="img-fluid float-left">
+                                                <h5 class="mb-1">Did you see the most beautiful sea in the world?</h5>
+                                                <small>07 Jan, 2016</small>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div><!-- end blog-list -->
+                            </div><!-- end widget -->
+                        
+                        </div><!-- end sidebar -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section>
+
+<?php require_once 'layout/footer.php' ?>
--- profile.php
+++ profile.php
@@ -1,5 +1,81 @@
 <?php
+
+use App\Models\Earning;
+use App\Models\User;
+
 require_once 'layout/header.php';
+
+$user = User::findUser('uname', $_SESSION['uname'])[0];
+$earning = Earning::findEarning($user['id'])[0];
+
 ?>
+<style>
+    .page-wrapper{
+        margin-top: 100px;
+    }
+</style>
+<div class="page-wrapper text-center center">
+
+<div class="row text-center">
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="sidebar">
+            <div class="widget-no-style">
+                <div class="newsletter-widget text-center align-self-center">
+                    <h3>Earnings in points</h3>
+                    <div class="row">
+                        <div class="col-lg-6"><b>Bref : </b></div>
+                        <div class="col-lg-6"><b>Bearn : </b></div>
+                    </div>
+                    <hr>
+                    <h5>Total : </h5>
+                </div><!-- end newsletter -->
+            </div>
+        </div><!-- end sidebar -->
+    </div><!-- end col -->
+
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div class="sidebar">
+            <div class="widget-no-style">
+                <div class="newsletter-widget text-center align-self-center">
+                    <h3>Earnings in cash</h3>
+                    <div class="row">
+                        <div class="col-lg-6"><b>Bref : </b></div>
+                        <div class="col-lg-6"><b>Bearn : </b></div>
+                    </div>
+                    <hr>
+                    <h5>Total : </h5>
+                </div><!-- end newsletter -->
+            </div>
+        </div><!-- end sidebar -->
+    </div><!-- end col -->
+</div>
+
+<div class="row text-center">
+<div class="col-lg-12">
+<p><strong>Referral link : https://bossearn.com/register.php?ref=<?php echo $user['ref'] ?></strong></p>
+</div>
+</div>
+
+    <div class="row">
+        <div class="col-lg-3"></div>
+
+        <div class="col-lg-6">
+            <form class="form-wrapper" method="POST">
+            <h4> Personal Details </h4>
+                <input type="text" class="form-control" placeholder="referral code" disabled value="<?php echo $user['ref'] ?>">
+                <input type="text" class="form-control" placeholder="First name" value="<?php echo $user['fname'] ?>" name="fn">
+                <input type="text" class="form-control" placeholder="Last name" value="<?php echo $user['lname'] ?>" name="ln">
+                <input type="text" class="form-control" placeholder="Country" disabled value="<?php echo $user['country'] ?>">
+                <input type="email" class="form-control" placeholder="Email address" disabled value="<?php echo $user['email'] ?>">
+                <input type="tel" class="form-control" placeholder="Phone" value="<?php echo $user['phone'] ?>" name="ph">
+                <input type="text" class="form-control" placeholder="Username" disabled value="<?php echo $user['uname'] ?>">
+                
+                <button type="submit" class="btn btn-primary"> update <i class="fa fa-arrow-right"></i></button>
+            </form>
+        </div>
+
+        <div class="col-lg-3"></div>
+    </div>
+</div>
 
 <?php require_once 'layout/footer.php'; ?>
