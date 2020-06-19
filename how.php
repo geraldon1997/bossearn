a8e7892cd5cd7097764b52148da5af3d637065ee
geraldon1997 <geraldon1997@gmail.com>
2020-06-19 03:39:06 -0400

    updating the how it works page

--- how.php
+++ how.php
@@ -7,62 +7,136 @@ require_once 'layout/header.php';
 <div class="page-wrapper">
     <div class="blog-title-area">
 
-        <span class="color-yellow"><a href="marketing-category.html" title="">How Bossearn works</a></span>
+        <span class="color-yellow"><a href="marketing-category.html" title="">Most Asked Questions</a></span>
 
-        <h3>Welcome to BOSSEARN !!!</h3>
+        <h3>What is BOSSEARN?</h3>
 
     </div><!-- end title -->
 
     <div class="blog-content">  
         <div class="pp">
-            <p>We are glad to have you back today. Kindly login with your registered username and password and start enjoying the opportunities for today.</p>
-            <p>Wishing you splendid stay ahead!</p>
+            <p>Bossearn concept is a news sharing platform tiers of Bossearn income forum incorporated with Corporate Affairs Commission with registration number 3089852. 
+                We source first hand news on happenings within the country and around the globe from National papers, Google news, local and foreign bloggers to benefit our erudite readers.</p>
+            <p></p>
 
-            <h3><strong>Why you should join us?</strong></h3>
+            <h3><strong>Why You Should Join Bossearn Earning Opportunity ?</strong></h3>
 
-            <ul>
-                <li>We pay 100% without referrals</li>
-                <li>We pay the Top  referral bonus ₦1,000</li>
-                <li>Two thousand Five hundred (2500)points for sign up bonus</li>
-                <li>Daily withdrawal of referral earnings, and many more</li>
-            </ul>
+            <p>As long as you make expenses everyday, you can take part in Bossearning opportunity.  This equal opportunity is for all. 
+                Either you are a Student, Hustler, Youth corp member, Stay-at-home mum, Unemployed, Fresh Graduate, House wife, Blogger, Self employed or anyone who wishes to make money online. 
+                Once you have an internet enabled mobile phone, you are good to go. 
+                Take advantage of your environment, families and friends, even Facebook, Whatsapp and other social media contact and make money for yourself.</p>
+
+            <p>Don’t allow the economy situation of the country to put your back on the ground. Seize the opportunity and make good use of it. Bossearning  Opportunity has come to stay for those who wish to earn online.
+                    Is Bossearning income forum Legitimate And Who is the Founder?
+                    For those doubting the legitimacy of this program and are quite on if this is on the fraud/scam way, the answer is capital NO! Bossearning program is not and can never be scam. 
+                    Bossearning Opportunity is a legitimate system, not a quick rich scheme and it’s registered under Corporate Affairs Commission (CAC) with the registration number 3089852</p>
+
+            <p>Bossearning income forum was created  , by ORJI EUNICE CHIDIMMA under the legal Protection of BOSSEARN INCOME FORUM A proficient Online business.</p>
+        </div><!-- end pp -->
+
+        <div class="pp">
+            <h3><strong>What is B.Point ?</strong></h3>
+
+            <p>BOSSEARN ADs SHARE is the Revenue we make through our Advertising partners. What’s minimum Earnings for Non Referrals ?
+                Minimum Pay for Activities Earners is not specific. Earn upto 10k point and convert it to your currency starting from a welcome bonus of 2500point</p>
+
+            <h3><strong>What Is B.REF ?</strong></h3>
+
+            <p>BOSSEARN REF is the SHARE made by those who refer into the system.
+                Must I refer Before I get paid ?
+                NO, you don’t have to refer before you get paid!</p>
+
+                <h3><strong>How do I Register people on Bossearn and get my Referral Bonus ?</strong></h3>
+
+                <p>On your dashboard click on referral to copy out your referral link  apply your referral link on your BROWSER then fill details of your prospect you will automatically get your referral bonus by using an activation code .</p>
 
         </div><!-- end pp -->
 
         <div class="pp">
-            <h3><strong>OUR SELLING POINTS</strong></h3>
+            <h1>Is Sponsored Post Compulsory?</h1>
 
+            <h4>YES</h4>
+
+            <p>Membership activities, posting, sponsored post sharing and commenting:
+Administrators, Editors, Moderators and robots reserve the right to approve or disapprove contents or comments posted on Bossearn. As a Bossearn member, you will only earn and get a revenue shared paid to your bank account when you abide by the following rules:
+Irrelevant contribution (comment) e.g: one word, hmmm, ok, lol, etc and a single line of the same sentence on all post or comments not related to subject matters are not allowed. They will be trashed and you will not earn.</p>
+
+            <p>Consistent irrelevant post of comments by a user and inappropriate behaviour may result in suspension or termination of membership and forfeiture of members unredeemed earnings.
+                Sponsored post sharing must corespond with the post date and must be shared as recommended.</p>
+
+            <h3><strong>How Can I share sponsored post ?</strong></h3>
+            <p>Go to your dashboard click on sponsored post you’ll see sponsored post for each day you want to share.
+                You’ll see a Text written there ‘CLICK HERE TO SHARE ‘
+                YOU ARE TO CLICK THERE IT WILL DIRECT YOU TO THE PARTICULAR MEDIA YOU CHOSE TO SHARE IT
+                That’s how you’ll be doing it daily for Effective recording.
+                YOU CAN ALSO FOLLOW THE FOUNDER OF BOSSEARN WITH THIS LINK.</p>
+
+            <h3><strong>Do I Get Paid without referrals </strong></h3>
+            <p>Yes. We share our activities Earnings (B.point) earnings every monthly from Top activity earners to low earners till the revenue gets Exhausted..</p>
+
+            <h3><strong>When do BOSSEARN pay ?</strong></h3>
+            <p>We pay B.ref Daily with Minimum of ₦3000 daily. We pay within 5hrs of Request.
+                While B.point  Earners are paid Every month as well  with Minimum 10,000 Gas Earnings</p>
+
+            <h3><strong>Why is my B.point earnings cleared to 0 ?</strong></h3>
+            <p>B.point earnings are BOSSEARN Ads shares we generate from our Advertising partners.
+                We Share our members part of the revenue made every DAY . So, we deduct all eligible members B.point Earnings. As far as you have a share in our Revenue all your Earnings will be wiped.</p>
+
+            <h3><strong>How to Register someone on Bossearn .</strong></h3>
+            <p>Go to your Dashboard ,Click on referral link on your dashboard to get your Referral link.
+                Copy it and start using it to refer your friends and family.
+                How do I Withdraw my Earnings on Bossearn
+                Referral earnings can be withdrawn anyday, anytime through wallet on your dashboard
+                While activities earnings can be withdrawn every month through B.point form on your Dashboard.</p>
+
+            <h3><strong>Ways I can pay for Registration on BOSSEARN ?</strong></h3>
             <ul class="check">
-                <li>REGISTRATION FEE A ONE TIME PAYMENT OF (₦2,500)</li>
-                <li>WE PAY EVERY DAY</li>
-                <li>REFERRING IS OPTIONAL</li>
-                <li>WE PAY THE TOP AFFILIATE COMMISSION (₦1,000)</li>
+                <li>Activation code</li>
+                <li>PayPal</li>
+                <li>Bitcoin</li>
             </ul>
 
-        </div><!-- end pp -->
+            <h3><strong>What’s activation code ?</strong></h3>
+            <p>Activation code is a E-pin or Voucher used for registration..
+                When you use an activation code and your referral link to register your prospect you will automatically get your referral bonus.</p>
 
-        <div class="pp">
-            <h3><strong>WAYS TO EARN LIKE A BOSS ON BOSSEARN(B.E)</strong></h3>
+            <h3><strong>How can foreigners Join ?</strong></h3>
+            <p>Foreigners can pay WITH PayPal or bitcoin and also we can pay them straight to there bank account.</p>
 
-            <h4>Let's calculate your estimated earnings</h4>
+            <p>Bossearn is worldwide Media welcome in Ghana, South Africa , USA,  India, Canada . E.T.C
+                Bossearn is worldwide Business with no limitation we have Dominate Ghana , South Africa with lot of Members Oversea.. USA,  India, Canada . E.T.C
+                You can pay via PayPal And Bitcoin contact our Marchant below for Payment</p>
 
+            <h4><strong>Buy Activation Code</strong></h4>
+            <h5><strong>The Use of Activation Code</strong></h5>
+            <p>An alternative way of making payment for registration is making use of our coupon code. You can get this through our accredited merchants. Your account will be approved within 10minutes</p>
+            
+            <h3><strong>BOSSEARN OFFICIAL ACTIVATION CODE MERCHANTS</strong></h3>
+            <p>WARNING! Do not pay to anyone who is not among the listed merchant, be guided. You can Whatsapp them, call them or add them up on Facebook.
+                You can pay via PayPal And Bitcoin contact our Marchant below for Processing.</p>
+
+            <h5><strong>How To Get Referrals</strong></h5>
+            <p>How To Get referrals To Register On Bossearn income Program is what I will be showing here today.</p>
+
+            <h5><strong>See how people advertise on Facebook groups to get Referrals you too can do the same</strong></h5>
             <ul class="check">
-                <li>Sign up bonus #2500</li>
-                <li>You make a comment on posts per week = 200 X7=1400 points</li>
-                <li>You share our sponsored post for a week = N500 X 7 = 3500 points</li>
-                <li>You refer 10persons in a week = N1,000 X 10 = 10,000 (Optional)</li>
-                <li>Daily login of 100 naira per day for 7 days = N100 X 7 = 700 points</li>
-                <li>sharing news to other sites form giftalworld attracts 20 naira per post. Let's say you share 10 posts daily. That's 200. For 7 days, that's 1400 points </li>
-                <li>Each news read gives 5 points = 5 X 7 = 35points </li>
+                <li>Join lots of Facebook group</li>
+                <li>Advertise there and collect people WhatsApp number</li>
+                <li>Add them to your WhatsApp group or Invite them to your group</li>
+                <li>Set your group only Admins to comment to avoid spam.
+                <li>Add at least over 150 people to your whatapp group daily trust me you are going to be making over 100k monthly</li>
+                <li>Tell them about the business and how it works.</li>
+                <li>Show them over 7-10 alert testimonies from our Facebook group.</li>
+                <li>You need to be doing it consistently even when you see no result, just know that there’s a beautiful day ahead..</li>
+                <li>Register them and start making cool cash daily..</li>
             </ul>
 
-            <h4>calculate how much you've made so far with just your smartphone</h4>
-            <p>That's over 20k for the week without referring</p>
-            <h4>WITH MY TRICK WHICH I WILL SHOW YOU, YOU SHOULD EARN OVER 30k FOR THE WEEK</h4>
-            <h5>Just imagine you, imagine yourself earning this (lets just say 40k) every week..  In a month, you're up with over 100k  </h5>
-            <p>And for withdrawal. That takes no time,  For example u get registered. All u have to do is go to bank details and put your account details. For withdrawal anytime</p>
-            <h5>Does Facebook, whatsapp or Ig pay you this daily or monthly</h5>
-            <h4>REMEMBER, It was all possible because of a #2500!!</h4>
+            <p>Today I will be showing you how to get referrals on Bossearn Program.
+Please note that this write up on how to get referrals on Bossearn Program. This is not something I should have shared online. But for any person that sees it,Kudos!</p>
+
+<p>If you are a serious minded person, you should start making over 20k every weekly on Bossearn after this. Do well to share it as well with whoever you love to see succeed in Bossearn.</p>
+
+
 
         </div><!-- end pp -->
     </div><!-- end content -->
