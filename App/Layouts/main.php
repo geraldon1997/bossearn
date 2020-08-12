bba66dd6df5d80db28f2f50a05ee3b2fa857744b
geraldon1997 <geraldon199@gmail.com>
2020-08-12 13:54:18 -0400

    added the layout file

--- App/Layouts/main.php
+++ App/Layouts/main.php
@@ -1,19 +1,202 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Site Metas -->
     <title>Bossearn</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="App/Assets/Images/logo.jpeg" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="App/Assets/Images/logo.jpeg">
+
+    <!-- Design fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="App/Assets/Css/bootstrap.css" rel="stylesheet">
+
+    <!-- FontAwesome Icons core CSS -->
+    <link href="App/Assets/Css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="App/Assets/Css/style.css" rel="stylesheet">
+
+    <!-- Animate styles for this template -->
+    <link href="App/Assets/Css/animate.css" rel="stylesheet">
+
+    <!-- Responsive styles for this template -->
+    <link href="App/Assets/Css/responsive.css" rel="stylesheet">
+
+    <!-- Colors for this template -->
+    <link href="App/Assets/Css/colors.css" rel="stylesheet">
+
+    <!-- Version Marketing CSS for this template -->
+    <link href="App/Assets/Css/version/marketing.css" rel="stylesheet">
+
+
+    <link rel='stylesheet' href='App/Assets/Css/share-button.min.css' type='text/css' media='all'/>
+    <!-- <script src="App/Assets/Js/jquery.min.js"></script> -->
+    
+
+
+<style>
+    #logo{
+        width: 100px;
+        height: 70px;
+    }
+    share-button{
+        background-color: gold;
+        color: black;
+        padding-left: 50px;
+        padding-right: 50px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+    #logout:hover{
+        cursor: pointer;
+    }
+    .page-wrapper{
+        margin-top: 100px;
+        margin-bottom: 100px;
+        text-align: center;
+    }
+    .tbl{
+        overflow: auto;
+    }
+    .market-header{
+        width: 100% !important;
+    }
+</style>
+
 </head>
 <body>
-<ul>
-    <li><a href="<?php echo HOME; ?>">index</a></li>
-    <li><a href="<?php echo HOW; ?>">how it works</a></li>
-    <li><a href="<?php echo CONTACT; ?>">contact</a></li>
-    <li><a href="<?php echo REGISTER; ?>">register</a></li>
-    <li><a href="<?php echo LOGIN; ?>">login</a></li>
-</ul>
 
-    {{content}}
+    <div id="wrapper">
+        <header class="market-header header">
+            <div class="container-fluid">
+                <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="/"><img src="App/Assets/Images/version/logo.jpeg" alt="bossearn" id="logo"></a>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mr-auto">
+
+                          	<li class="nav-item">
+                                <a class="nav-link" href="/">Home</a>
+                            </li>
+
+                        </ul>
+                        <!-- <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="text" placeholder="How may I help?">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form> -->
+                    </div>
+                </nav>
+            </div><!-- end container-fluid -->
+        </header><!-- end market-header -->
+
+        {{content}}
+
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        
+                        <div class="copyright">&copy; Bossearn 2020</div>
+                    </div>
+                </div>
+            </div><!-- end container -->
+        </footer><!-- end footer -->
+
+        <div class="dmtop"></div>
+        
+    </div><!-- end wrapper -->
+
+    <!-- Core JavaScript
+    ================================================== -->
+    <script src="App/Assets/Js/share-button.min.js"></script>
+    <script src="App/Assets/Js/jquery.min.js"></script>
+    <script src="App/Assets/Js/tether.min.js"></script>
+    <script src="App/Assets/Js/bootstrap.min.js"></script>
+    <script src="App/Assets/Js/animate.js"></script>
+    <script src="App/Assets/Js/custom.js"></script>
+    <script>
+
+        $( document ).ready(function() {
+            
+            new ShareButton({
+                networks: {
+                    whatsapp: {
+                        before: function(e) {
+                            this.url = $(e).attr('data-url'),
+                            this.description = $(e).attr('data-description')
+                        },
+                        after: function(e) {
+                            pid_val = $(e).attr('post-id')
+
+                            $.ajax({
+                            url: 'https://bossearn.com/addshare.php',
+                            type: 'POST',
+                            data : { 
+                                pid : pid_val
+                                }
+                            })
+                        }
+                    },
+                    facebook: {
+                        before: function(e) {
+                            this.url = $(e).attr('data-url'),
+                            this.description = $(e).attr('data-description')
+                        },
+                        after: function(e) {
+                            pid_val = $(e).attr('post-id')
+                            
+                            $.ajax({
+                            url: 'http://bossearn.test/addshare.php',
+                            type: 'POST',
+                            data : {
+                                pid : pid_val
+                                }
+                            })
+                            
+                        }
+                    },
+                    twitter: {
+                        before: function(e) {
+                            this.url = $(e).attr('data-url'),
+                            this.description = $(e).attr('data-description')
+                            
+                        },
+                        after: function(e) {;
+                            pid_val = $(e).attr('post-id');
+                            
+                            $.ajax({
+                            url: 'https://bossearn.com/addshare.php',
+                            type: 'POST',
+                            data : {
+                                pid : pid_val
+                                }
+                            })
+                              
+                        }
+                    }
+                },
+                ui: {
+                    flyout: 'middle bottom'
+                    }
+            });
+        });
+
+    </script>
 </body>
 </html>
